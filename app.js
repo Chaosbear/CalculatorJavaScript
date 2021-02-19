509649eb0efc1205a7f98ffaf8e65b3d2374d743
Chaosbear <sukrit.chatmeeboon@gmail.com>
2021-02-19 23:45:48 +0700

    use eval to calculate

--- app.js
+++ app.js
@@ -1,69 +1,48 @@
-
-// class Calculator {
-//     constructor(inputBeforeOperator, inputAfterOperator) {
-//         this.inputBeforeOperator = inputBeforeOperator
-//         this.inputAfterOperator = inputAfterOperator
-//     }
-
-//     updateDisplay(existNum,num) {
-        
-//     }
-
-// }
-const numButton = document.querySelectorAll(".number-button")
-const operatorButton = document.querySelectorAll(".operator-button")
 const displayInput = document.querySelector("#displayInput")
 const displayOutput = document.querySelector("#displayOutput")
+const numButton = document.querySelectorAll(".number-button")
+const operatorButton = document.querySelectorAll(".operator-button")
+const deleteButton = document.querySelector("#delete")
+const clearButton = document.querySelector("#clear")
+const equalButton = document.querySelector("#equal")
 
 let previousOperand = ""
 let currentOperand = ""
-let operator = undefined
 let result
-const stack = []
+let inputString =""
+let isEqual = false
 
 function addNumber(numElem) {
-    displayInput.innerText += numElem.innerText
-    currentOperand += numElem.innerText
+  if (isEqual === true) {
+    displayInput.innerText = ""
+    inputString = ""
+    isEqual = false
+  }
+  displayInput.innerText += numElem.innerText
+  inputString += numElem.innerText
 }
 
 function addOperation(operatorElem) {
 
-    if (["+","-","×","÷"].includes(displayInput.innerText.slice(-1))) {
+    isEqual = false
+    
+    if (["+","-","×","÷"].includes(inputString.slice(-1))) {
         displayInput.innerText = displayInput.innerText.slice(0, -1) + operatorElem.innerText
+        inputString = inputString.slice(0,-1) + operatorElem.getAttribute("id")
         return
     }
-    if (previousOperand === "") {
-        previousOperand = currentOperand
-    }
-    else {previousOperand = result.toString()}
     
-    currentOperand = ""
     displayInput.innerText += operatorElem.innerText
-    operator = operatorElem.getAttribute("id")
+    inputString += operatorElem.getAttribute("id")
 }
 
 function compute() {
-    const prev = parseFloat(previousOperand)
-    const current = parseFloat(currentOperand)
-    if (isNaN(prev) || isNaN(current)) return
-    switch (operator) {
-      case 'plus':
-        result = prev + current
-        break
-      case 'minus':
-        result = prev - current
-        break
-      case 'multiply':
-        result = prev * current
-        break
-      case 'divide':
-        result = prev / current
-        break
-      default:
-        return
-    }
-
-    displayOutput.innerText = result
+  if (inputString === "") {
+    displayOutput.innerText = ""
+    return
+  }
+  result = eval(inputString)
+  displayOutput.innerText = result
 }
 
 numButton.forEach((elem) => {
@@ -79,4 +58,21 @@ operatorButton.forEach((elem) => {
     })
 })
 
+deleteButton.addEventListener("click", function() {
+  displayInput.innerText = displayInput.innerText.slice(0, -1)
+  inputString = inputString.slice(0, -1)
+  compute()
+})
 
+clearButton.addEventListener("click", function() {
+  displayInput.innerText = ""
+  inputString = "" 
+  compute()
+})
+
+equalButton.addEventListener("click", function() {
+  displayInput.innerText = result.toString()
+  displayOutput.innerText = ""
+  inputString = result.toString()
+  isEqual = true
+})
